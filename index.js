9fc6cade16596395eba2af4454a9200312a03512
Gustav Young <gus.santana@icloud.com>
2020-12-22 01:55:58 -0600

    fix pls_nueva

--- index.js
+++ index.js
@@ -1188,7 +1188,7 @@ function playPlayList() {
                 // omxplayer = spawn('/usr/bin/omxplayer', ['-o', configs.output, '-b', '--no-keys', '-g', sharedday + '/' + playlist.files[playlist.currentIndex]]);
             }
             omx.once('end', function() {
-                if (streaming && playlist.currentIndex === playlist.files.length){
+                if (streaming && playlist.currentIndex + playlist.files.length === playlist.files.length){
                     logAndPrint('info', 'recargando playlist ' + (playlist.currentIndex) + (playlist.files.length));
                     playNext();
                     stopPlay().then(function(data) {
